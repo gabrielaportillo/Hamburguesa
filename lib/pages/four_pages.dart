0cb9850ab2441afaa25605e15775bafb1095e5cf
FlutLab.io <>
2021-06-01 17:06:32 +0000

    Autosave

--- lib/pages/four_pages.dart
+++ lib/pages/four_pages.dart
@@ -1,121 +1,187 @@
 import 'package:flutter/material.dart';
 
-class FourPage extends StatefulWidget {
-  @override
-  _FourPageState createState() => _FourPageState();
-}
-
-class _FourPageState extends State<FourPage> {
+class FourPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: 300, minWidth: 200),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 80.0,
-              ),
-              Container(
-                color: Colors.red[800],
-                height: 200,
-                width: 350,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 200,
-                width: 250,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.deepOrangeAccent[400],
-                      Colors.deepOrangeAccent,
-                      Colors.deepOrangeAccent[100],
-                    ],
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                  ),
-                ),
-                child: Container(
-                  padding: EdgeInsets.only(top: 35.0, bottom: 16),
-                  child: Text(
-                    'Somos tu mejor opcion',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 50,
-                width: 100,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.amber,
-                      Colors.amber[400],
-                      Colors.amber[300],
-                    ],
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                  ),
-                ),
-                child: Text(
-                  'Bienvenido',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              InkWell(
-                onTap: () {
-                  print('sky');
-                },
-                child: Container(
-                  width: 110,
-                  height: 110,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30.0),
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.brown[50],
-                        Colors.brown[100],
-                        Colors.brown[200],
-                      ],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                    ),
-                  ),
-                  child: ImageIcon(
-                    AssetImage("assets/images/corazon.png"),
-                    size: 100,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App Portillo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Inicio(title: 'Gabriela Portillo'),
+    ); //App Material
+  } //Constructor
+} //Clase MiApphat
+
+class Inicio extends StatefulWidget {
+  Inicio({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _InicioState createState() => _InicioState();
+}
+class _InicioState extends State<Inicio> {
+  String elegirValor;
+  List listItem = [
+    '1997',
+    '1998',
+    '1999',
+    '2000',
+    '2001',
+    '2002',
+    '2003'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFF44336),
+        centerTitle: true,
+        title: Text(
+          'OXXO',
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
       ),
-    );
-  }
-}
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            LimitedBox(
+              maxWidth: 350.0,
+              maxHeight: 300.0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  'https://raw.githubusercontent.com/gabrielaportillo/mis_imagenes/main/oxxo2.png',
+                  fit: BoxFit.cover,
+                ),
+              ), //fin de Imagen 
+            ),
+
+            SizedBox(
+              height: 16,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Color(0xFFF44336),
+                  ),
+                ),
+                labelText: 'Nombre del usuario',
+              ),
+            ), //fin del campo de texto
+            SizedBox(
+              height: 16,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Color(0xFFF44336),
+                  ),
+                ),
+                labelText: 'Apellidos del usuario',
+              ),
+            ), //fin de campo de texto
+            SizedBox(
+              height: 16,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Color(0xFFF44336),
+                  ),
+                ),
+                labelText: 'Correo electronico',
+              ),
+            ), //fin de textField
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: <Widget>[
+                new Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Color(0xFFF44336),
+                        ),
+                      ),
+                      labelText: 'Fecha de nacimiento (Dia y Mes)',
+                    ), //fin de Decoración de Textfield
+                  ), //fin del TexField
+                ),//fin de flexible
+                SizedBox(
+                  width: 16,
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  decoration: BoxDecoration(border: Border.all(color: Color(0xFFF44336), width: 1), borderRadius: BorderRadius.circular(4)),
+                  child: DropdownButton(
+                    hint: Text(
+                      "Año: ",
+                      style: TextStyle(
+                        color: Color(0xFF9E9E9E),
+                      ),
+                    ),
+                    dropdownColor: Color(0xFF5BDBDBD),
+                    icon: Icon(Icons.expand_more_rounded),
+                    iconSize: 20,
+                    underline: SizedBox(),
+                    value: elegirValor,
+                    onChanged: (newValue) {
+                      setState(() {
+                        elegirValor = newValue;
+                      });
+                    },
+                    items: listItem.map((valueItem) {
+                      return DropdownMenuItem(
+                        value: valueItem,
+                        child: Text(valueItem),
+                      ); //fin de Dropdown Menu Item
+                    }).toList(),
+                  ), //fin de Dropdown
+                ), //fin de Contenedor de Dropdown
+              ], //fin de Widgets dentro de Row
+            ), //fin del cuarto Row
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  label: Text('Iniciar sesion'),
+                  icon: Icon(Icons.check_rounded),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    onPrimary: Colors.white,
+                    minimumSize: Size(140, 50),
+                  ),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  label: Text('Cancelar'),
+                  icon: Icon(Icons.close_rounded),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey,
+                    onPrimary: Colors.white,
+                    minimumSize: Size(140, 50),
+                  ),
+                )
+              ], //fin de hijos de Row
+            ), //fin de row
+          ], //fin de Widgets dentro de Columna
+        ), //fin de Columna Principal
+      ),//fin de body
+    ); //fin del Scaffold
+  } //fin del Constructor
+} //fin de Clase Inicio
