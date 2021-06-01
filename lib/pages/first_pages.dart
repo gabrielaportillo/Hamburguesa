8e495a60ad47157024d4b0187516d4c8482df5ea
FlutLab.io <>
2021-06-01 16:37:45 +0000

    Autosave

--- lib/pages/first_pages.dart
+++ lib/pages/first_pages.dart
@@ -4,14 +4,7 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Dana Romero Widgets App',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[200],
-      ),
+      
       backgroundColor: Colors.white,
       body: Container(
         child: Center(
