a882af1798fc3c438a5d32e4327de8cc38a587dd
FlutLab.io <>
2021-06-01 16:36:20 +0000

    Autosave

--- lib/pages/first_pages.dart
+++ lib/pages/first_pages.dart
@@ -3,13 +3,35 @@ import 'package:flutter/material.dart';
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Primera Pagina',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Dana Romero Widgets App',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[200],
+      ),
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Center(
+          child: Column(
+            // center the children
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                child: new Image.asset(
+                  'assets/images/oxxo2.png',
+                ),
+              ),
+              Text(
+                "Soy la primer pagina!",
+                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
+              )
+            ],
+          ),
         ),
       ),
     );
-  }
-}
+  } // widget
+} // first page
